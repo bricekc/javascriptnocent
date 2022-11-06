b90e1afe46db2cb1f7c0a6fd175e0c666bbf2b46
Kuca Brice <brice.kuca@etudiant.univ-reims.fr>
2022-11-06 12:06:16 +0100

    fin du script pour table de multiplication

---
+++ table de multiplication/script/script.js
@@ -0,0 +1,12 @@
+let table = Number(prompt("choisissez une table de multiplication"));
+let h1 = document.createElement("h1");
+document.body.appendChild(h1);
+document.querySelector("h1").textContent = "Table de multiplication de "+table;
+for(let i = 0;i<11;i++)
+{
+    let res = table * i;
+    let div = document.createElement("div" );
+    div.classList.add(i);
+    document.body.appendChild(div);
+    document.getElementsByClassName(i)[0].textContent = table+" * "+i+" = "+res;
+}
